c556c683515e7b72b06c87a0c54faba717a36f94
Mariana <mifologic@gmail.com>
2024-03-27 00:23:25 +0300

    Update files

--- read_xlsx_file.py
+++ read_xlsx_file.py
@@ -4,6 +4,6 @@ df = pd.read_excel('file_example_XLSX_10.xlsx')
 print('Заголовки файла')
 print(df.columns.values.tolist())  # вывод заголовков в виде списка
 print('\n Данные колонки по имени')
-print(df['Size'].values.tolist())  # вывод данных из определённой колонки
+print(df['Category'].values.tolist())  # вывод данных из определённой колонки
 print('\n Все значения в виде списка объектов')
 print(df.to_dict('records'))  #  вывод значений файла в виде списка объектов
